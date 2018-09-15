376483c2d735a6a35fd3f9ebc0a42f81fa394e6f
nice <996099092@qq.com>
2018-09-15 12:02:30 +0800

    增加addArticle方法

--- src/main/java/com/nice/dao/UserDao.java
+++ src/main/java/com/nice/dao/UserDao.java
@@ -1,7 +1,12 @@
 package com.nice.dao;
 
+import com.nice.entity.Article;
+
 /**
  * @author nice
  */
 public interface UserDao {
+
+    Article addArticle(Article article);
+
 }
--- src/main/java/com/nice/dao/impl/UserDaoImpl.java
+++ src/main/java/com/nice/dao/impl/UserDaoImpl.java
@@ -1,9 +1,14 @@
 package com.nice.dao.impl;
 
 import com.nice.dao.UserDao;
+import com.nice.entity.Article;
 
 /**
  * @author nice
  */
 public class UserDaoImpl implements UserDao {
+    @Override
+    public Article addArticle(Article article) {
+        return article;
+    }
 }
